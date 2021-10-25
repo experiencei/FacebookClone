c7df248c68386e151cd190f5143ac186cddbe160
experiencei <ayelojahighbee01@gmail.com>
2021-10-25 15:11:51 +0100

    using tailwind css to style it

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,7 +1,18 @@
+import Image from "next/image";
+
 function Header() {
     return (
         <div>
             
+
+            <div>
+                <Image
+                    src="https://links.papareact.com/5me"
+                    width={40}
+                    height={40}
+                    layout="fixed"
+                />
+            </div>
         </div>
     )
 }
