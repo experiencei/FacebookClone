da8e25a8d9117e86688c1b1e131e4dd1251c462a
experiencei <ayelojahighbee01@gmail.com>
2021-10-25 18:18:47 +0100

    wrapping with provider

--- components/sidebar/Sidebar.js
+++ components/sidebar/Sidebar.js
@@ -13,7 +13,13 @@ function Sidebar() {
 
     return (
         <div className="p-2 mt-5 max-w-[600px] xl:min-w-[300px]">
-            <SidebarRow Icon={}/>
+            <SidebarRow Icon={UsersIcon} title="Friends"/>
+            <SidebarRow Icon={UserGroupIcon} title="Groups"/>
+            <SidebarRow Icon={ShoppingBagIcon} title="Marketplace"/>
+            <SidebarRow Icon={DesktopComputerIcon} title="Watch"/>
+            <SidebarRow Icon={CalendarIcon} title="Events"/>
+            <SidebarRow Icon={ClockIcon} title="Memories"/>
+            <SidebarRow Icon={ChevronDownIcon} title="See More"/>
         </div>
     )
 }
