f9a6c493205291539e1e61ea99f3a0777c34c336
experiencei <ayelojahighbee01@gmail.com>
2021-10-25 19:42:15 +0100

    wrapping with provider

--- components/stories/Stories.js
+++ components/stories/Stories.js
@@ -28,6 +28,7 @@ function Stories() {
         <div className="flex justify-center space-x-3 mx-auto">
             {stories.map( story => (
                 <StoryCard
+                    key={story.src}
                     name={story.name}
                     src={story.src}
                     profile={story.profile}
