c623c27a2aafaf00b05055f33c149545041317ba
experiencei <ayelojahighbee01@gmail.com>
2021-10-25 16:33:17 +0100

    using tailwind css to style it

--- components/headerIcon/HeaderIcon.js
+++ components/headerIcon/HeaderIcon.js
@@ -1,8 +1,8 @@
 function HeaderIcon({Icon}) {
     return (
         <div className="flex items-center cursor-pointer md:px-10 sm:h-14
-        md:hover:bg-gray-100 rounded-xl active:border-b-2 active:border-blue-500">
-            <Icon  className="h-5"/>
+        md:hover:bg-gray-100 rounded-xl active:border-b-2 active:border-blue-500 group">
+            <Icon  className="h-5 group-hover:text-blue-500"/>
         </div>
     )
 }
