bb827455f35f443390b38d7a7f7613f989495d8f
experiencei <ayelojahighbee01@gmail.com>
2021-10-25 16:49:47 +0100

    using tailwind css to style it

--- components/header/Header.js
+++ components/header/Header.js
@@ -22,7 +22,7 @@ function Header() {
           <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
                  <SearchIcon  className="h-6 text-gray-600"/>
                  <input 
-                    className="flex ml-2 items-center bg-transparent
+                    className="hidden md:inline-flex  flex ml-2 items-center bg-transparent
                     outline-none placeholder-gray-500 flex-shrink"
                     type="text"
                     placeholder="Search facebook"/>
@@ -31,7 +31,7 @@ function Header() {
             
             <div  className="flex justify-center flex-grow">
                 <div className="flex space-x-6 md:space-x-2">
-                       <HeaderIcon Icon={HomeIcon}/>
+                       <HeaderIcon active Icon={HomeIcon}/>
                        <HeaderIcon Icon={FlagIcon}/>
                        <HeaderIcon Icon={PlayIcon}/>
                        <HeaderIcon Icon={ShoppingCartIcon}/>
@@ -39,6 +39,10 @@ function Header() {
                 </div>
             </div>
 
+            <div  className="flex items-center sm:space-x-2 justify-end">
+                {/* <Image/> */}
+                <p className="whitespace-nowrap font-semibold pr-3">Experience</p>
+            </div>
 
         </div>
     )
