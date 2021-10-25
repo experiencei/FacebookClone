7665a9a276a8875f43392ec398279b75587e2b68
experiencei <ayelojahighbee01@gmail.com>
2021-10-25 16:11:24 +0100

    using tailwind css to style it

--- components/header/Header.js
+++ components/header/Header.js
@@ -18,7 +18,7 @@ function Header() {
                     layout="fixed"
                 />
           <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
-                 <SearchIcon  className="h-6"/>
+                 <SearchIcon  className="h-6 text-gray-600"/>
                  <input 
                     className="flex ml-2 items-center bg-transparent
                     outline-none placeholder-gray-500"
