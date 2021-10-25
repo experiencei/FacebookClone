f574cb2033218014b3835bf70701a97ae6880726
experiencei <ayelojahighbee01@gmail.com>
2021-10-25 20:47:25 +0100

    wrapping with provider

--- components/feed/Feed.js
+++ components/feed/Feed.js
@@ -2,7 +2,7 @@ import Stories from "../stories/Stories"
 
 function Feed() {
     return (
-        <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
+        <div className="flex flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
             <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
                 <Stories/>
             </div>
--- components/header/Header.js
+++ components/header/Header.js
@@ -22,7 +22,7 @@ function Header() {
                     height={40}
                     layout="fixed"
                 />
-          <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <div className="hidden md:inline-flex ml-2 items-center rounded-full bg-gray-100 p-2">
                  <SearchIcon  className="h-6 text-gray-600"/>
                  <input 
                     className="hidden md:inline-flex  flex ml-2 items-center bg-transparent
