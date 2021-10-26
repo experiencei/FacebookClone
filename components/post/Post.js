5da73567854471df976a4cca113cd658bed3db68
experiencei <ayelojahighbee01@gmail.com>
2021-10-26 13:05:42 +0100

    working on input of the file image

--- components/post/Post.js
+++ components/post/Post.js
@@ -3,7 +3,7 @@ function Post({name , message, email , timestamp , image , postImage }) {
     return (
         <div className="flex flex-col">
             <div>
-                 <div>
+                 <div className="flex items-center space-x-2">
                         <img
                         src={image}
                         className="rounded-full"
@@ -12,7 +12,7 @@ function Post({name , message, email , timestamp , image , postImage }) {
                         alt="post"
                        />
                   <div>
-                 <p>{name}</p>
+                 <p className="font-medium">{name}</p>
                         <p className="text-x5 text-gray-400">
                            {new Date(timestamp?.toDate()).toLocaleString()}
                         </p>
