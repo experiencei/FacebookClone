7eef2a2017a58bd2d7800b27190251456253ae7c
experiencei <ayelojahighbee01@gmail.com>
2021-10-26 13:06:42 +0100

    working on input of the file image

--- components/post/Post.js
+++ components/post/Post.js
@@ -2,7 +2,7 @@
 function Post({name , message, email , timestamp , image , postImage }) {
     return (
         <div className="flex flex-col">
-            <div>
+            <div className="p-5 bg-white mt-5 rounded-t-2xl shadow-sm">
                  <div className="flex items-center space-x-2">
                         <img
                         src={image}
