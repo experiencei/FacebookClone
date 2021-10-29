4206403bf49a06e50a91e22c3d9350722844b07b
experiencei <ayelojahighbee01@gmail.com>
2021-10-29 09:31:34 +0100

    working on the posts component

--- components/posts/Posts.js
+++ components/posts/Posts.js
@@ -11,22 +11,22 @@ function Posts( { posts }) {
         <div>
            {    realtimePosts?
                 realtimePosts?.docs.map(post => <Post
-                 key={post.id}
-                name={post.data().name}
-                message={post.data().message}
-                email={post.data().email}
-                timestamp={post.data().timestamp}
+                 key={post?.id}
+                name={post?.data()?.name}
+                message={post?.data()?.message}
+                email={post?.data()?.email}
+                timestamp={post?.data()?.timestamp}
                 image={post?.data()?.image}
                 postImage={post?.data()?.postImage}
             />) : (
                 posts.map((post) => (<Post
-                 key={post.id}
-                name={post.data().name}
-                message={post.data().message}
-                email={post.data().email}
-                timestamp={post.data().timestamp}
-                image={post?.data()?.image}
-                postImage={post?.data()?.postImage}
+                key={post?.id}
+                // name={post?.data()?.name}
+                // message={post?.data()?.message}
+                // email={post?.data()?.email}
+                // timestamp={post?.data()?.timestamp}
+                // image={post?.data()?.image}
+                // postImage={post?.data()?.postImage}
             />))
             )}
         </div>
